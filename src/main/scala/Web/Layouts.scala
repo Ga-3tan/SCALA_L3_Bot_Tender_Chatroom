a399acd41d7c9988d3b5bc24904605cae09e51b4
JunodChristophe <49147076+JunodChristophe@users.noreply.github.com>
2022-05-06 16:40:43 +0200

    Step2 done

--- src/main/scala/Web/Layouts.scala
+++ src/main/scala/Web/Layouts.scala
@@ -1,11 +1,41 @@
 package Web
 
-import scalatags.Text.all._
+import scalatags.Text.all.*
 import scalatags.Text.tags2
 
 /**
  * Assembles the method used to layout ScalaTags
  */
 object Layouts:
-    // You can use it to store your methods to generate ScalaTags.
+    def homePage(): scalatags.Text.TypedTag[String] = {
+      html(
+        head(
+          link(rel:="stylesheet", href:="/static/resources/css/main.css"),
+          script(src:="/static/resources/js/main.js")
+        ),
+        body(
+          tag("nav")(
+            a(cls := "nav-brand", "Bot-tender"),
+            div(cls := "nav-item",
+              a("Log in")
+            )
+          ),
+          div(cls := "content",
+            div(id := "boardMessage",
+              div(cls := "msg",
+                tag("center")("Please wait, the messages are loading !"),
+                span(cls := "author", "TestAuthor"),
+                span(cls := "msg-content", "Hello world !")
+              )
+            ),
+            form(id := "msgForm", onsubmit:="submitMessageForm(); return false",
+              div(id := "errorDiv", cls:="errorMsg", "Test error"),
+              label(`for`:="messageInput", "Your message"),
+              input(id := "messageInput", `type`:="text", placeholder:="Write your message"),
+              input(`type`:="submit")
+            )
+          )
+        )
+      )
+    }
 end Layouts
